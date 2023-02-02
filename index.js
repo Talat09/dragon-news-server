a4909c8f26ab77b063908a7969229d3f83f84b8b
Talat09 <talat181008312@gmail.com>
2023-02-03 01:06:43 +0600

    loaded category data

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const categories = require("./data/categories.json");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("news api is running");
+});
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+app.listen(port, () => {
+  console.log("Dragon server is running on port: ", port);
+});
